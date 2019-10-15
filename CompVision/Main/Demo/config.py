c59a21e6401761dff0a5c8e69fcbb5aaa56277c5
StefanPushkov <stefan.pushkov@icloud.com>
2019-10-15 23:07:47 +0300

    Initial commit

---
+++ CompVision/Main/Demo/config.py
@@ -0,0 +1,13 @@
+import os
+
+raw_data = "/tData/"
+BASE_DIR = 'Main'
+
+VideoOut_file = 'C:/Storage/DDG_FaceRecog/CompVision/Main/Data/out.avi'
+
+def get_base_dir_by_name(name):
+    path = os.getcwd()
+    lastchar = path.find(name) + len(name)
+    return os.getcwd()[0:lastchar]
+
+base_dir = get_base_dir_by_name(BASE_DIR).replace("\\","/")
